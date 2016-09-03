686b11e04927972c6e0bd2b512189c403e0930c0
lizhaofu <lizhaofu0215@163.com>
2016-09-03 17:35:47 +0800

    First  java code

---
+++ src/HelloDate.java
@@ -0,0 +1,14 @@
+//¿Ó’—∏¶ 2016.09.03
+//HelloDate.java
+
+import java.util.*;
+public class HelloDate {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, it is: ");
+		System.out.println(new Date());
+
+	}
+
+}
